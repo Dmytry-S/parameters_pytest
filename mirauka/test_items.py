f5c5fe7f8f15ce5ce639f6f274f58e62bd908a88
Dmytry-S <47301088+Dmytry-S@users.noreply.github.com>
2020-06-09 22:55:19 +0300

    Delete test_items.py

--- mirauka/test_items.py
+++
@@ -1,9 +0,0 @@
-import time
-
-
-def test_guest_should_see_add_to_basket_button (browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
-    browser.get(link)
-    # time.sleep(30)
-    add_to_basket_button = browser.find_element_by_css_selector(".btn.btn-add-to-basket")
-    assert add_to_basket_button.is_displayed()
