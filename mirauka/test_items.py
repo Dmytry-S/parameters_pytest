67661e2fe8a3f96e07db170591329a68b0f74437
Dmytry-S <sviridoffd@gmail.com>
2020-06-09 23:04:25 +0300

    9.06.20

--- mirauka/test_items.py
+++ mirauka/test_items.py
@@ -1,12 +1,14 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import time
 
 link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 
 
 def test_presence_of_buy_button(browser):
     browser.get(link)
+    time.sleep(10)
     WebDriverWait(browser, 10).until(
         EC.presence_of_element_located((By.CLASS_NAME, "btn-add-to-basket"))
     )
